5a85c5cf68e191190dd252cd3b9c09217cdadd37
Sergey Gulaga <ry6ens@gmail.com>
2021-08-12 13:30:22 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './sass/main.scss';
+import menu from './menu.json';
+import itemsTemplate from './templates/gallery-items.hbs';
+
+console.log(menu)
+
+const galleryRef = document.querySelector('.js-menu');
+
+const markup = itemsTemplate(menu);
+galleryRef.insertAdjacentHTML('beforeend', markup);
