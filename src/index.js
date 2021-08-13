7b307b08a781fa381bdf587ad006f5c6dbb03c97
Sergey Gulaga <ry6ens@gmail.com>
2021-08-13 11:26:48 +0300

    commit2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import './sass/main.scss';
 import menu from './menu.json';
 import itemsTemplate from './templates/gallery-items.hbs';
-
-console.log(menu)
 
 const galleryRef = document.querySelector('.js-menu');
 
 const markup = itemsTemplate(menu);
 galleryRef.insertAdjacentHTML('beforeend', markup);
+
+// Change color theme
+const page = document.querySelector('.page')
+const themeButton = document.querySelector('.theme-switch__toggle')
+const theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+themeButton.onclick = function() {
+    const themeChecked = document.getElementById('theme-switch-toggle')
+    if (themeChecked.checked) {
+        page.classList.toggle(theme.LIGHT);
+        localStorage.setItem('theme', theme.LIGHT)
+    } else {
+        page.classList.toggle(theme.DARK);
+        localStorage.setItem('theme', theme.DARK)
+    }  
+  };
